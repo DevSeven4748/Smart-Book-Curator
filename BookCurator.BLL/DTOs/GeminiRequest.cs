c988e6f05a92aa6ec499ae3cbba265ba74f41a89
Metin Seven <metinseven4748@gmail.com>
2026-07-11 19:09:02 +0300

    feat: add Gemini request/response DTOs

---
+++ BookCurator.BLL/DTOs/GeminiRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BookCurator.BLL.DTOs
+{
+    //request shapes
+    public class GeminiRequest
+    {
+        [JsonPropertyName("contents")]
+        public List<GeminiContent> Contents { get; set; } = new();
+    }
+
+    public class GeminiContent 
+    {
+        [JsonPropertyName("parts")]
+        public List<GeminiPart> Parts { get; set; } = new();
+    }
+
+    public class GeminiPart
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = string.Empty;
+    }
+
+    // response shapes
+    public class GeminiResponse
+    {
+        [JsonPropertyName("candidates")]
+        public List<GeminiCandidate> Candidates { get; set; } = new();
+    }
+
+    public class GeminiCandidate
+    {
+        [JsonPropertyName("content")]
+        public GeminiContent Content { get; set; } = new();
+    }
+
+
+
+
+
+
+
+
+
+}
