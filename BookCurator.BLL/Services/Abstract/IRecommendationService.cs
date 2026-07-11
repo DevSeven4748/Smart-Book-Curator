06ff8666a9770d3639b23ad02ee6b7faedfb4e35
Metin Seven <metinseven4748@gmail.com>
2026-07-12 00:05:44 +0300

    feat: add IRecommendationService interface

---
+++ BookCurator.BLL/Services/Abstract/IRecommendationService.cs
@@ -0,0 +1,12 @@
+﻿using BookCurator.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCurator.BLL.Services.Abstract
+{
+    public interface IRecommendationService
+    {
+        Task<string> GetRecommendationAsync(IEnumerable<Book> books, string? mood);
+    }
+}
