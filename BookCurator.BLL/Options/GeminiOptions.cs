81a8e4da82c290ded102a0c2e5ec3fe7821f72e2
Metin Seven <metinseven4748@gmail.com>
2026-07-11 18:38:16 +0300

    feat: add Gemini config section and GeminiOptions

---
+++ BookCurator.BLL/Options/GeminiOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCurator.BLL.Options
+{
+    public class GeminiOptions
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string BaseUrl { get; set; } = string.Empty;
+    }
+}
