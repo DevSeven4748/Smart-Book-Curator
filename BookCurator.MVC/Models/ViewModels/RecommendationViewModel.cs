a4f90cf97ed5b1ffb7cbb727a91bcb8dc0bcc5ba
Metin Seven <metinseven4748@gmail.com>
2026-07-12 21:03:35 +0300

    feat: add RecommendationViewModel

---
+++ BookCurator.MVC/Models/ViewModels/RecommendationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookCurator.MVC.Models.ViewModels
+{
+    public class RecommendationViewModel
+    {
+        public string? Mood { get; set; }
+        public string? Result { get; set; }
+        public int BookCount { get; set; }
+        public bool IsLoading { get; set; }
+    }
+}
