bc2a025deb5c1225b13c051796fcfd6fe9ab47d0
Metin Seven <metinseven4748@gmail.com>
2026-07-14 18:33:33 +0300

    feat: add ITvShowService and TvShowService

---
+++ BookCurator.BLL/Services/Abstract/ITvShowService.cs
@@ -0,0 +1,13 @@
+﻿using BookCurator.DAL.Entities;
+
+namespace BookCurator.BLL.Services.Abstract
+{
+    public interface ITvShowService
+    {
+        Task<IEnumerable<TvShow>> GetAllTvShowsAsync();
+        Task<TvShow?> GetTvShowByIdAsync(int id);
+        Task AddTvShowAsync(TvShow tvShow);
+        Task UpdateTvShowAsync(TvShow tvShow);
+        Task DeleteTvShowAsync(int id);
+    }
+}
---
+++ BookCurator.BLL/Services/Concrete/TvShowService.cs
@@ -0,0 +1,37 @@
+﻿using BookCurator.BLL.Services.Abstract;
+using BookCurator.DAL.Entities;
+using BookCurator.DAL.Repositories.Abstract;
+
+namespace BookCurator.BLL.Services.Concrete
+{
+    public class TvShowService(ITvShowRepository repository) : ITvShowService
+    {
+        public async Task<IEnumerable<TvShow>> GetAllTvShowsAsync() =>
+            await repository.GetAllAsync();
+
+        public async Task<TvShow?> GetTvShowByIdAsync(int id) =>
+            await repository.GetByIdAsync(id);
+
+        public async Task AddTvShowAsync(TvShow tvShow)
+        {
+            await repository.AddAsync(tvShow);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task UpdateTvShowAsync(TvShow tvShow)
+        {
+            repository.Update(tvShow);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteTvShowAsync(int id)
+        {
+            var tvShow = await repository.GetByIdAsync(id);
+            if (tvShow != null)
+            {
+                repository.Delete(tvShow);
+                await repository.SaveChangesAsync();
+            }
+        }
+    }
+}
