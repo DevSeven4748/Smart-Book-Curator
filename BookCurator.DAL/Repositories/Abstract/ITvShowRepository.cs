f0e23821eede46784bff602c97c9d097e655f778
Metin Seven <metinseven4748@gmail.com>
2026-07-14 18:17:34 +0300

    feat; add ITvShowRepository and TvShowRepository

---
+++ BookCurator.DAL/Repositories/Abstract/ITvShowRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookCurator.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCurator.DAL.Repositories.Abstract
+{
+    public interface ITvShowRepository
+    {
+        Task<IEnumerable<TvShow>> GetAllAsync();
+        Task<TvShow?> GetByIdAsync(int id);
+        Task AddAsync(TvShow tvShow);
+        void Update(TvShow tvShow);
+        void Delete(TvShow tvShow);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ BookCurator.DAL/Repositories/Concrete/TvShowRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookCurator.DAL.Repositories.Abstract;
+using BookCurator.DAL.Entities;
+
+namespace BookCurator.DAL.Repositories.Concrete
+{
+    public class TvShowRepository(AppDbContext context) : ITvShowRepository
+    {
+        public async Task<IEnumerable<TvShow>> GetAllAsync() => await context.TvShows.OrderByDescending(m => m.DateAdded).ToListAsync();
+
+        public async Task<TvShow?> GetByIdAsync(int id) => await context.TvShows.FindAsync(id);
+
+        public async Task AddAsync(TvShow tvShow) => await context.TvShows.AddAsync(tvShow);
+
+        public void Delete(TvShow tvShow) => context.TvShows.Remove(tvShow);
+
+        public void Update(TvShow tvShow) => context.TvShows.Update(tvShow);
+
+        public async Task<bool> SaveChangesAsync() => (await context.SaveChangesAsync()) > 0;
+    }
+}
