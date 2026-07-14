e3b9ac4c27b0212f73357f5fba4af4542873ca92
Metin Seven <metinseven4748@gmail.com>
2026-07-14 18:28:08 +0300

    feat: add IMovieService and MovieService

---
+++ BookCurator.BLL/Services/Concrete/MovieService.cs
@@ -0,0 +1,37 @@
+﻿using BookCurator.BLL.Services.Abstract;
+using BookCurator.DAL.Entities;
+using BookCurator.DAL.Repositories.Abstract;
+
+namespace BookCurator.BLL.Services.Concrete
+{
+    public class MovieService(IMovieRepository repository) : IMovieService
+    {
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync() =>
+            await repository.GetAllAsync();
+
+        public async Task<Movie?> GetMovieByIdAsync(int id) =>
+            await repository.GetByIdAsync(id);
+
+        public async Task AddMovieAsync(Movie movie)
+        {
+            await repository.AddAsync(movie);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task UpdateMovieAsync(Movie movie)
+        {
+            repository.Update(movie);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteMovieAsync(int id)
+        {
+            var movie = await repository.GetByIdAsync(id);
+            if (movie != null)
+            {
+                repository.Delete(movie);
+                await repository.SaveChangesAsync();
+            }
+        }
+    }
+}
