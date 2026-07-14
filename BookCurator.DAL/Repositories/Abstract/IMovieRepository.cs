3177ec01df220aa48d8823a5eee5dccd3a0abe87
Metin Seven <metinseven4748@gmail.com>
2026-07-14 18:06:25 +0300

    feat: add IMovieRepository and MovieRepository

---
+++ BookCurator.DAL/Repositories/Abstract/IMovieRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookCurator.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCurator.DAL.Repositories.Abstract
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<Movie?> GetByIdAsync(int id);
+        Task AddAsync(Movie movie);
+        void Update(Movie movie);
+        void Delete(Movie movie);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ BookCurator.DAL/Repositories/Concrete/MovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookCurator.DAL.Repositories.Abstract;
+using BookCurator.DAL.Entities;
+
+namespace BookCurator.DAL.Repositories.Concrete
+{
+    public class MovieRepository(AppDbContext context) : IMovieRepository
+    {
+        public async Task<IEnumerable<Movie>> GetAllAsync() => await context.Movies.OrderByDescending(m => m.DateAdded).ToListAsync();
+
+        public async Task<Movie?> GetByIdAsync(int id) => await context.Movies.FindAsync(id);
+
+        public async Task AddAsync(Movie movie) => await context.Movies.AddAsync(movie);
+
+        public void Delete(Movie movie) => context.Movies.Remove(movie);
+
+        public void Update(Movie movie) => context.Movies.Update(movie);
+
+        public async Task<bool> SaveChangesAsync() => (await context.SaveChangesAsync()) > 0;
+    }
+}
