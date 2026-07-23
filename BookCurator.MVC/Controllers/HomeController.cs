b24753910ac068c60496f590b5a7045a66dc47ee
Metin Seven <metinseven4748@gmail.com>
2026-07-23 18:42:22 +0300

    feat: HomeController gathers cross-media dashboard data

--- BookCurator.MVC/Controllers/HomeController.cs
+++ BookCurator.MVC/Controllers/HomeController.cs
@@ -1,15 +1,34 @@
 using BookCurator.BLL.Services.Abstract;
 using BookCurator.Models;
+using BookCurator.MVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BookCurator.Controllers;
 
-public class HomeController : Controller
+public class HomeController(IBookService bookService, IMovieService movieService, ITvShowService tvShowService) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var books = await bookService.GetAllBooksAsync();
+        var movies = await movieService.GetAllMoviesAsync();
+        var shows = await tvShowService.GetAllTvShowsAsync();
+
+        var recent = new List<RecentItem>();
+
+        recent.AddRange(books.Select(b => new RecentItem { MediaType = "Book", Title = b.Title, Status = b.Status.ToString(), DateAdded = b.DateAdded }));
+        recent.AddRange(movies.Select(m => new RecentItem { MediaType = "Movie", Title = m.Title, Status = m.Status.ToString(), DateAdded = m.DateAdded }));
+        recent.AddRange(shows.Select(s => new RecentItem { MediaType = "TV Show", Title = s.Title, Status = s.Status.ToString(), DateAdded = s.DateAdded }));
+
+        var model = new DashboardViewModel
+        {
+            BookCount = books.Count(),
+            MovieCount = movies.Count(),
+            TvShowCount = shows.Count(),
+            RecentItems = recent.OrderByDescending(r => r.DateAdded).Take(9).ToList()
+        };
+
+        return View(model);
     }
 
     public IActionResult Privacy()
